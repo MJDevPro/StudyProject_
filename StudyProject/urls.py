44321eaf4652e36ac4e44bba11c84ef8e5f9872f
mjdevpro <mjdevpro@gmail.com>
2022-01-26 22:48:26 +0500

    New Chenges!

--- StudyProject/urls.py
+++ StudyProject/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('logout/',include('django.contrib.auth.urls'),name='logout'),
     path('', include('base.urls')),
 ]
